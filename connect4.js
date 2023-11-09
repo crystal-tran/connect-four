35242f884209b833177c0e63f0c9b7bf7d7a8525
Crystal Tran <c.t.tran95@gmail.com>
2023-11-08 20:00:38 -0500

    assigned variables to horiz,vert,diagDL,diagDR

--- connect4.js
+++ connect4.js
@@ -105,16 +105,17 @@ function checkForWin() {
    * returns true if all are legal coordinates for a cell & all cells match
    * currPlayer
    */
+  // Check four cells to see if they're all legal & all color of current
+  // player
   function _win(cells) {
-    return cells.every(([y,x]) => {
+    console.log("calling _win:", cells);
+    return cells.every(([y,x]) =>
       y >= 0 &&
       y < HEIGHT &&
       x >= 0 &&
       x < WIDTH &&
       board[y][x] === currPlayer
-    })
-    // TODO: Check four cells to see if they're all legal & all color of current
-    // player
+    );
 
 
   }
@@ -130,9 +131,9 @@ function checkForWin() {
       // [ [y, x], [y, x], [y, x], [y, x] ]
 
       let horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
-      let vert;
-      let diagDL;
-      let diagDR;
+      let vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
+      let diagDL= [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
+      let diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
 
       // find winner (only checking each win-possibility as needed)
       if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
